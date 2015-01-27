a24176e37a2198b55da31623820c15e5de5441a0
Wen Li <wen.li@ucl.ac.uk>
2015-01-27 15:52:18 +0000

    Fix datapusher-srv dependency in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=[],
+    install_requires=["ckanserviceprovider==0.0.2", "messytables>=0.12.0", "python-slugify", "Requests", "argparse"],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
